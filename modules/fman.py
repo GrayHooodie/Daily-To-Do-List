a51ae5803a88b9c0e5479cc136b0e674e664c0f5
GrayHoodie <matrala0419@gmail.com>
2026-01-28 09:46:12 -0600

    Fixed default items conflict

--- modules/fman.py
+++ modules/fman.py
@@ -135,10 +135,10 @@ def autoload() -> None:
 					glob.todo = [line.strip('\n') for line in f.readlines()]
 				glob.todo.pop()
 			else:
-				default_items()
 				glob.todo += open_list(open_file["name"])
 				glob.todo.pop()
 				rm_crossed()
+				default_items()
 				append_postponed()
 			with open(path.join(glob.progfiles, "lastopen"), 'w') as f:
 				f.write(f"{glob.date}.todo\n%d\n")
@@ -152,11 +152,23 @@ def autoload() -> None:
 	return None
 
 def default_items() -> None:
-	with open(path.join(glob.conffiles, "daily-default"), 'r') as f:
-		items = [line.strip("\n") for line in f.readlines()]
-	if len(items):
-		glob.todo = items
+    rm_old_default_items()
+    default_items = read_default_items()
+    for i in range(len(default_items)):
+        glob.todo.insert(i, default_items[i])
 	return None
+
+def rm_old_default_items() -> None:
+	default_items = read_default_items()
+    for item in glob.todo:
+        if item in default_items:
+            glob.todo.remove(item)
+    return None
+
+def read_default_items() -> list[str]:
+    with open(path.join(glob.conffiles, "daily-default"), 'r') as f:
+	    items = [line.strip("\n") for line in f.readlines()]
+    return items
 
 def rm_crossed() -> None:
 	uncrossed = []
