c3ff4887b00d6fbeda61720b1e274561af74dce0
GrayHoodie <matrala0419@gmail.com>
2026-02-02 02:15:39 -0600

    install.sh fix1

--- install.sh
+++ install.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
 echo "Installing..."
-if [ $(which dtdl) == ~/.local/bin/dtdl ]
-then
+test_path = $(which dtdl)
+if [ $test_path == ~/.local/bin/dtdl ]; then
     cp dist/dtdl ~/.local/bin/dtdl
 else
     sudo --prompt="Enter password to make program available to all users. Otherwise, type Ctrl+c:" cp dist/dtdl /usr/bin 2> /dev/null
-    if [ $? == 1 ]
-    then
+    if [ $? == 1 ]; then
         mkdir -p ~/.local/bin
         echo $PATH | grep ~/.local/bin > /dev/null 2>&1
-        if [ $? == 1 ]
-        then
-            if [[ "$(echo $SHELL)" =~ "bash" ]]
-            then
+        if [ $? == 1 ]; then
+            if [[ "$(echo $SHELL)" =~ "bash" ]]; then
                 echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
                 source ~/.bashrc
-            elif [[ "$(echo $SHELL)" =~ "zsh" ]]
-            then
+            elif [[ "$(echo $SHELL)" =~ "zsh" ]]; then
                 echo "export PATH=$PATH:~/.local/bin" >> ~/.zshrc
                 source ~/.zshrc
-            elif [[ "$(echo $SHELL)" =~ "fish" ]]
-            then
+            elif [[ "$(echo $SHELL)" =~ "fish" ]]; then
                 echo "set -U fish_user_paths ~/.local/bin" >> ~/.config/fish/config.fish
             else
                 echo "Unknown shell. Aborting."
@@ -35,8 +30,7 @@ fi
 python3 setup.py
 
 which dtdl > /dev/null 2>&1
-if [ $? == 1 ]
-then
+if [ $? == 1 ]; then
     echo "Error. Not installed."
 else
     echo "Installed to $(which dtdl)"
