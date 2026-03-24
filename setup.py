c5d24e37b195229deaa125e67cd55f01f9101824
GrayHoodie <matrala0419@gmail.com>
2026-03-24 03:11:22 -0500

    added __init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dtdl',
+    version='1.1.5',
+    description='A simple to-do list CLI',
+    author='Matt L',
+    author_email='matrala0419@gmail.com',
+    packages=['dtdl', 'modules'],
+    install_requires=[],
+)
