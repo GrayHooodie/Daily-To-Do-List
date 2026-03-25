13855955f44be85467444cb802dc11a53f49dc00
GrayHoodie <matrala0419@gmail.com>
2026-03-25 02:38:39 -0500

    Made install script more robust

--- install.sh
+++ install.sh
@@ -6,12 +6,33 @@ if ! [ -f ./dist/dtdl ]; then
 fi
 
 echo "Installing..."
-ls ~/.local/bin/ | grep dtdl > /dev/null 2>&1
+copied=0
+which dtdl > /dev/null 2>&1
 if [ $? = 0 ]; then
-    cp dist/dtdl ~/.local/bin/
-else
+	updt='U'
+	if [ $(which dtdl | grep '^/home') ]; then
+		read -p "Another version of dtdl is detected at '$(which dtdl)'. Would you like to update it? [y/n]: " updt
+	elif [ $(which dtdl | grep '^/usr') ]; then
+		read -p "Another version of dtdl is detected at '$(which dtdl)'. Would you like to update it? (requires superuser, i.e. your password) [y/n]: " updt
+	fi
+	updt=$(echo $updt | tr '[:lower:]' '[:upper:]')
+    while [ "$updt" != 'Y' ] && [ "$updt" != 'N' ]; do 
+		read -p "Enter 'y' or 'n': " updt
+		updt=$(echo $updt | tr '[:lower:]' '[:upper:]')
+	done
+	if [ $updt = 'Y' ] && [ $(which dtdl | grep '^/home') ]; then
+    	cp dist/dtdl $(which dtdl)
+		copied=1
+	elif [ $updt = 'Y' ]; then
+		sudo cp dist/dtdl $(which dtdl)
+		if [ $? = 0 ]; then
+			copied=1
+		fi
+	fi
+fi
+if [ $copied = 0 ]; then
     whchpth='U'
-    read -p "Would you like to make the program available to all users? (requires superuser, i.e. your password) [y/n]:" whchpth
+    read -p "Would you like to make the program available to all users? (requires superuser, i.e. your password) [y/n]: " whchpth
 	whchpth=$(echo $whchpth | tr '[:lower:]' '[:upper:]')
     while [ "$whchpth" != 'Y' ] && [ "$whchpth" != 'N' ]; do 
 		read -p "Enter 'y' or 'n': " whchpth
@@ -19,6 +40,9 @@ else
 	done
     if [ $whchpth = 'Y' ]; then
         sudo cp dist/dtdl /usr/bin/ 2> /dev/null
+		if [ $? = 0 ]; then
+			copied=1
+		fi
     fi
     if [ $? != 0 ] || [ $whchpth = 'N' ]; then
         mkdir -p ~/.local/bin/
@@ -38,6 +62,7 @@ else
             fi
         fi
         cp dist/dtdl ~/.local/bin/
+		copied=1
     fi
 fi
 
@@ -45,6 +70,9 @@ which dtdl > /dev/null 2>&1
 if [ $? != 0 ]; then
     echo "Error. Is '~/.local/bin' in your PATH variable?"
     exit 1
+elif [ $copied = 0 ]; then
+	echo "Error. Program not installed."
+	exit 1
 else
     echo "Installed to $(which dtdl)"
 fi
